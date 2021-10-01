955951851ec648a19054a844359b878143b36dd4
NABIN777 <nirajan.khanal777@gmail.com>
2021-10-01 09:52:19 +0545

    modified

--- register.py
+++ register.py
@@ -1,52 +1,130 @@
-from tkinter import*
-from tkcalendar import DateEntry
+from tkinter import *
+from PIL import ImageTk,Image
+import sqlite3
 from tkinter import messagebox
-root = Tk()
-root.maxsize(500,500)
-root.minsize(500,500)
-root.configure(background='peru')
-def msg():
-    select=var.get()
-    if select==0:
-        response=messagebox.showwarning("FAILED","PLEASE FILL ALL CONTENT")
-    elif (entry_2.index("end") == 0):
-        messagebox.askyesno('Missing E-mail', 'you sure wanna miss it?')
-    else:
-      response = messagebox.showinfo("Thank you", "YOUR REGISTER IS DONE")
+import subprocess
+from tkcalendar import calendar_
+from tkcalendar.dateentry import DateEntry
+
+root=Tk()
+root.title('Register')
+root.geometry('400x580')
+root.resizable(False, False)
+root.config(bg="#111d5e")
+
+#----------------------------------DATABASE---------------------------------------
+
+
+conn=sqlite3.connect('register.db')
+
+c=conn.cursor()
+
+#######table
+
+# c.execute(""" CREATE TABLE login(
+        
+#         name String NOT NULL,
+#         username String PRIMARY KEY,
+#         adress String NOT NULL,
+#         DOB Integer NOT NULL,
+#         password String NOT NULL,
+
+
+#         )""")
+def run_register():
+   root.destroy()
+   subprocess.call(["python","register.py"])
+
+
+
+def run_login():
+    root.destroy()
+    subprocess.call(["python","login.py"])
+
+
+def register():
+    
+    conn=sqlite3.connect('register.db')
+
+    c=conn.cursor()
+
+    c.execute('SELECT 1 FROM login WHERE username = ?',(username_entry.get(),))
+
+    while len(c.fetchall()) > 0:
+
+        messagebox.showerror("Error","Username already exists")
+        
+        run_register()
+
+        c.execute('SELECT 1 FROM login WHERE username = ?',(username_entry.get(),))
+
+
+    c.execute("INSERT INTO login VALUES(:name, :username, :adress, :password)",
+        
+                {
+                    "name": name_entry.get(),
+                    "username": username_entry.get(),
+                    "address": address_entry.get(),
+                    "DOB": DOB_entry.get(),
+                    "password": password_entry.get(),
+                    
+                }
+        
+        )
+    
+    messagebox.showinfo("register","register sucessfully")
+    
+    name_entry.delete(0,END)
+    username_entry.delete(0,END)
+    address_entry.delete(0,END)
+    DOB_entry.delete(0,END)
+    password_entry.delete(0,END)
+
+    conn.commit()
+    conn.close()
+    
+    run_login()
+
+register_text_label=Label(root,text="CREATE ACCOUNT",font=(Canvas,25),bg="#111d5e",fg="dark red",relief=GROOVE,borderwidth=4)
+register_text_label.place(x=55,y=15)
+
+
+name_label=Label(root,bg="#111d5e",text="Name:",font=(Canvas,12),fg="red")
+name_label.place(x=4,y=250)
+name_entry=Entry(root,font=Canvas,width=35,bg="white")
+name_entry.place(x=4,y=275)
+
+username_label=Label(root,bg="#111d5e",text="Username:",font=(Canvas,12),fg="red")
+username_label.place(x=4,y=305)
+username_entry=Entry(root,font=Canvas,width=35,bg="white")
+username_entry.place(x=4,y=330)
+
+address_label=Label(root,bg="#111d5e",text="Address:",font=(Canvas,12),fg="red")
+address_label.place(x=4,y=360)
+address_entry=Entry(root,font=Canvas,width=35,bg="white")
+address_entry.place(x=4,y=385)
+
+
+DOB_label=Label(root,bg="#111d5e",text="DOB:",font=(Canvas,12),fg="red")
+DOB_label.place(x=4,y=415)
+DOB_entry=DateEntry(root,width=61,date_pattern="dd/mm/Y",bg="grey")
+DOB_entry.place(x=4,y=440,height=30)
+
+password_label=Label(root,bg="#111d5e",text="Password:",font=(Canvas,12),fg="red")
+password_label.place(x=4,y=470)
+password_entry=Entry(root,font=Canvas,width=35,bg="white")
+password_entry.place(x=4,y=495)
+
+
+register_button=Button(root,text="Sign Up",bg="green",borderwidth=4,command=register,relief=GROOVE,fg="WHITE",width=20)
+register_button.place(x=110,y=530)
 
 
 
 
-root.title("Registration Form")
-label_0 = Label(root, text="Registration form",borderwidth=4,relief=GROOVE,width=20,font=("bold", 20),bg="green",fg="white")
-label_0.place(x=90,y=53)
-label_1 = Label(root, text="FullName:",width=20,borderwidth=4,relief=GROOVE,font=("bold", 10),bg="peru")
-label_1.place(x=50,y=130)
-entry_1 = Entry(root,fg="green")
-entry_1.place(x=240,y=130)
-label_2 = Label(root, text="Email:",borderwidth=4,relief=GROOVE,width=20,font=("bold", 10),bg="peru")
-label_2.place(x=50,y=180)
-entry_2 = Entry(root)
-entry_2.place(x=240,y=180)
-label_3 = Label(root, text="Gender:",borderwidth=4,relief=GROOVE,width=20,font=("bold", 10),bg="peru")
-label_3.place(x=50,y=230)
-var = IntVar()
-Radiobutton(root, text="Male",padx = 5, variable=var, value=1,bg="orange").place(x=235,y=230)
-Radiobutton(root, text="Female",padx = 5, variable=var, value=2,bg="orange").place(x=290,y=230)
-Radiobutton(root,text="other",padx=5,variable=var,  value=3,bg="orange").place(x=355,y=230)
-label_4 = Label(root, text="Age:",borderwidth=4,relief=GROOVE,width=20,font=("bold", 10),bg="peru")
-label_4.place(x=50,y=280)
-entry_2 = Entry(root)
-entry_2.place(x=240,y=280)
-l4 = Label(root,borderwidth=4,relief=GROOVE, text="DOB:",width=20,font=("bold",10),bg='peru')
-l4.place(x=50,y=330)
 
-dob = DateEntry(root, width=27, background='brown', foreground='white',date_pattern='dd/mm/Y', borderwidth=3)
-dob.place(x=240,y=330)
-f1=Frame(root,bg="RED",borderwidth=6,relief=GROOVE)
-f1.pack(side=BOTTOM)
-btn=Button(f1, text='Submit',width=20,bg='BLUE',fg='white',command=msg)
-btn.pack()
-s=Label(text="©Nirajan,NEPAL",font=("italic",10))
-s.place(x=395 ,y=480)
+
+conn.commit()
+conn.close()
+
 root.mainloop()
